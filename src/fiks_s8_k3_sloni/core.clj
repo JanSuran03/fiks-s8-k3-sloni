066b2e3c326de7c496e1b230c2bebf8ce16bf03e
JanSuran03 <62071148+JanSuran03@users.noreply.github.com>
2021-12-19 00:33:19 +0100

    Input processing and first thoughts.

--- src/fiks_s8_k3_sloni/core.clj
+++ src/fiks_s8_k3_sloni/core.clj
@@ -1,6 +1,25 @@
-(ns fiks-s8-k3-sloni.core)
+(ns fiks-s8-k3-sloni.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn read-and-process-input []
+  (->> "input.txt" slurp
+       str/split-lines
+       rest
+       (map #(as-> % input-line
+                   (str/split input-line #"\ ")
+                   (map read-string input-line)))))
+
+(defn solve [[width height x y moves]]
+  (println width height x y moves)
+  (let [x-left (- x moves)
+        x-right (+ x moves)
+        y-bottom (- y moves)
+        y-top (+ y moves)]
+    [x-left
+     x-right
+     y-bottom
+     y-top]))
+
+(defn -main [& _args]
+  (let [processed-input (read-and-process-input)]
+    (solve (first processed-input))))

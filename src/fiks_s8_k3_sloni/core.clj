a8b5f134c994e5222079da03ab25a19f1eac6081
JanSuran03 <62071148+JanSuran03@users.noreply.github.com>
2021-12-26 15:43:04 +0100

    Some WIP...

--- src/fiks_s8_k3_sloni/core.clj
+++ src/fiks_s8_k3_sloni/core.clj
@@ -9,16 +9,35 @@
                    (str/split input-line #"\ ")
                    (map read-string input-line)))))
 
-(defn solve [[width height x y moves]]
-  (println width height x y moves)
+(defmulti analyze-intersection-part-1
+          (fn [[x-left x-right] [y-bottom y-top] [width height]]
+            [(> x-left 0)                                   ; :left-out?
+             (< x-right width)                              ; :right-out?
+             (> y-bottom 0)                                 ; :bottom-out?
+             (< y-top height)]))                            ; :top-out?
+
+(defmethod analyze-intersection-part-1 [true true true true]
+  [& s]
+  (println "pepeha"))
+
+(defn rhombus-vertices-and-quarters [x y moves]
   (let [x-left (- x moves)
         x-right (+ x moves)
         y-bottom (- y moves)
-        y-top (+ y moves)]
-    [x-left
-     x-right
-     y-bottom
-     y-top]))
+        y-top (+ y moves)
+        horizontal-line [x-left x-right]
+        vertical-line [y-bottom y-top]
+        quad-1-triangle [[(inc x) (inc y)] [(dec x-right) (inc y)] [(inc x) (dec y-top)]]
+        quad-2-triangle [[(dec x) (inc y)] [(dec x) (dec y-top)] [(inc x-left) (inc y)]]
+        quad-3-triangle [[(dec x) (dec y)] [(inc x-left) (dec y)] [(dec x) (inc y-bottom)]]
+        quad-4-triangle [[(inc x) (dec y)] [(inc x) (inc y-bottom)] [(dec x-right) (dec y)]]]
+    [[x-left x-right y-bottom y-top]
+     [quad-1-triangle quad-2-triangle quad-3-triangle quad-4-triangle]]))
+
+(defn solve [[width height x y moves]]
+  (println width height x y moves)
+  (let [{:keys [x-left x-right y-bottom y-top]} (rhombus-vertices-and-quarters x y moves)]
+    (analyze-intersection-part-1 [x-left x-right] [y-bottom y-top] [width height])))
 
 (defn -main [& _args]
   (let [processed-input (read-and-process-input)]

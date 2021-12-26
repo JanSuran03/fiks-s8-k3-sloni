288659a575bd0e2b8d21032a5297104cebf20391
JanSuran03 <62071148+JanSuran03@users.noreply.github.com>
2021-12-26 18:43:21 +0100

    Normalizing function (flipping along x, y or x+y) for rhombus triangluar seqments working.

--- src/fiks_s8_k3_sloni/core.clj
+++ src/fiks_s8_k3_sloni/core.clj
@@ -9,18 +9,21 @@
                    (str/split input-line #"\ ")
                    (map read-string input-line)))))
 
-(defmulti analyze-intersection-part-1
-          (fn [[x-left x-right] [y-bottom y-top] [width height]]
-            [(> x-left 0)                                   ; :left-out?
-             (< x-right width)                              ; :right-out?
-             (> y-bottom 0)                                 ; :bottom-out?
-             (< y-top height)]))                            ; :top-out?
+(defn pprint-array [[width height x y quad-triangle]]
+  (let [s (set quad-triangle)]
+    (newline)
+    (doseq [row (reverse (range width))]
+      (doseq [col (range height)]
+        (printf "%3s" (cond (and (= x row)
+                                 (= y col)) "X"
+                            (contains? s [row col]) "O"
+                            :else \u00b7)))
+      (newline))))
 
-(defmethod analyze-intersection-part-1 [true true true true]
-  [& s]
-  (println "pepeha"))
+(defmulti compute-intersection
+          (fn [[width height center-x center-y [[x1 y1] [x2 y2] [x3 y3]]]]))
 
-(defn rhombus-vertices-and-quarters [x y moves]
+(defn rhombus-quarters-and-diagonals [x y moves]
   (let [x-left (- x moves)
         x-right (+ x moves)
         y-bottom (- y moves)
@@ -31,13 +34,36 @@
         quad-2-triangle [[(dec x) (inc y)] [(dec x) (dec y-top)] [(inc x-left) (inc y)]]
         quad-3-triangle [[(dec x) (dec y)] [(inc x-left) (dec y)] [(dec x) (inc y-bottom)]]
         quad-4-triangle [[(inc x) (dec y)] [(inc x) (inc y-bottom)] [(dec x-right) (dec y)]]]
-    [[x-left x-right y-bottom y-top]
-     [quad-1-triangle quad-2-triangle quad-3-triangle quad-4-triangle]]))
+    [[quad-1-triangle quad-2-triangle quad-3-triangle quad-4-triangle]
+     [horizontal-line vertical-line]]))
+
+(defn flip-along-x-axis [width height center-x center-y [[x1 y1] [x2 y2] [x3 y3]]]
+  (let [[new-center-y new-y1 new-y2 new-y3] (map (fn [x]
+                                                   (- height 1 x)) [center-y y1 y2 y3])]
+    [width height center-x new-center-y [[x1 new-y1] [x2 new-y2] [x3 new-y3]]]))
+
+(defn flip-along-y-axis [width height center-x center-y [[x1 y1] [x2 y2] [x3 y3]]]
+  (let [[new-center-x new-x1 new-x2 new-x3] (map (fn [x]
+                                                   (- width 1 x)) [center-x x1 x2 x3])]
+    [width height new-center-x center-y [[new-x1 y1] [new-x2 y2] [new-x3 y3]]]))
+
 
 (defn solve [[width height x y moves]]
   (println width height x y moves)
-  (let [{:keys [x-left x-right y-bottom y-top]} (rhombus-vertices-and-quarters x y moves)]
-    (analyze-intersection-part-1 [x-left x-right] [y-bottom y-top] [width height])))
+  (let [[[quad-1-triangle quad-2-triangle quad-3-triangle quad-4-triangle]
+         [horizontal-line vertical-line]] (rhombus-quarters-and-diagonals x y moves)
+        normalized-quad-1 [width height x y quad-1-triangle]
+        normalized-quad-2-along-x (flip-along-y-axis width height x y quad-2-triangle)
+        normalized-quad-3-along-x-y (->> [width height x y quad-3-triangle]
+                                         (apply flip-along-x-axis)
+                                         (apply flip-along-y-axis))
+        normalized-quad-4-along-y (flip-along-x-axis width height x y quad-4-triangle)]
+    (pprint-array normalized-quad-1)
+    (pprint-array normalized-quad-4-along-y)
+    (pprint-array normalized-quad-2-along-x)
+    (pprint-array normalized-quad-3-along-x-y)
+    (doseq [x [normalized-quad-1 normalized-quad-2-along-x normalized-quad-3-along-x-y normalized-quad-4-along-y]]
+      (println x))))
 
 (defn -main [& _args]
   (let [processed-input (read-and-process-input)]

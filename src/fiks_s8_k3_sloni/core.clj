3771ae165e42d6fc574121d90c0d573bccebaaf4
JanSuran03 <62071148+JanSuran03@users.noreply.github.com>
2021-12-28 20:49:53 +0100

    WIP odd/even moves, forgot to commit.

--- src/fiks_s8_k3_sloni/core.clj
+++ src/fiks_s8_k3_sloni/core.clj
@@ -3,8 +3,16 @@
 
 (def dot-in-the-middle \u00b7)
 
+(def evenness-complement {:even :odd
+                          :odd  :even})
+
+(defn even-or-odd-field [[start-x start-y] [target-x target-y] evenness]
+  (if (even? (+ start-x start-y target-x target-y))
+    evenness
+    (evenness-complement evenness)))
+
 (defn read-and-process-input []
-  (->> "input.txt" slurp
+  (->> "sample.txt" slurp
        str/split-lines
        rest
        (map #(as-> % input-line
@@ -22,6 +30,20 @@
                             :else dot-in-the-middle)))
       (newline))))
 
+(defn odd-and-even
+  "Based on the evenness in the starting corner and the number of squares on one
+  side of the triangle, returns a hash-map of number of squares visitable exactly
+  after the given number of moves and the other one, taking the starting corner
+  evenness into consideration."
+  [corner-evenness overhang]
+  (let [corner-evenness-compl (evenness-complement corner-evenness)
+        corner-evenness-fields (int (Math/pow (quot (inc overhang) 2) 2))
+        overhang-half (quot overhang 2)
+        ;; divided by 2, but multiplied by 2 right after
+        not-corner-evenness-fields (* overhang-half (inc overhang-half))]
+    {corner-evenness       corner-evenness-fields
+     corner-evenness-compl not-corner-evenness-fields}))
+
 (defmulti compute-intersection
           "Takes data of a rectangle and a normalized triangle so that the triangle should
            be in the first quadrant from the center and all the coordinates positive:
@@ -32,14 +54,12 @@
 
            x2 > rest
            y3 > rest"
-          (fn [[height width center-x center-y [[x1 y1] [max-x y2] [x3 max-y]] :as in]]
-            (print-array in)
+          (fn [corner-evenness [height width center-x center-y [[x1 y1] [max-x y2] [x3 max-y]] :as in]]
             (let [max-row (dec height)
                   max-col (dec width)
                   x-out-of-field (> max-x max-row)
                   y-out-of-field (> max-y max-col)
                   ret [x-out-of-field y-out-of-field]]
-              ;(println ret)
               ret)))
 
 (defn test-arr [arr]
@@ -51,7 +71,7 @@
      2))
 
 (defmethod compute-intersection [false false]               ; the entire triangle is inside the rectangle
-  [[height width center-x center-y [[x1 y1] [x2 y2] [x3 y3]]]]
+  [corner-evenness [height width center-x center-y [[x1 y1] [x2 y2] [x3 y3]]]]
   ;    x · · · ·    obviously the flat of the triangle here is (apply + (range 1 (+ 1 5)))
   ;    · · · · ·    => (apply + (range 1 (+ 1 n)))
   ;    · · · · ·    { Σ(1 -> k): k = n } == (n^2 + n)/2
@@ -61,52 +81,48 @@
     (dotted-triangle-flat overhang)))
 
 (defmethod compute-intersection [false true]                ; X-greatest vertex INside, Y-greatest OUTside
-  [[height width center-x center-y [[x1 y1] [x2 y2] [x3 y3]]]]
+  [corner-evenness [height width center-x center-y [[x1 y1] [x2 y2] [x3 y3]]]]
   (let [inside-overhang (inc (- x2 x1))
         outside-overhang (- y3 (dec width))]
     (- (dotted-triangle-flat inside-overhang)
        (dotted-triangle-flat outside-overhang))))
 
 (defmethod compute-intersection [true false]                ; X-greatest vertex OUTside, Y-greatest OUTside
-  [[height width center-x center-y [[x1 y1] [x2 y2] [x3 y3]]]]
+  [corner-evenness [height width center-x center-y [[x1 y1] [x2 y2] [x3 y3]]]]
   (let [inside-overhang (inc (- x2 x1))
         outside-overhang (- x2 (dec height))]
     (- (dotted-triangle-flat inside-overhang)
        (dotted-triangle-flat outside-overhang))))
 
-(defn test-all []
-  (let [data1 [12 10 5 6 [[6 7] [11 7] [6 12]]]
-        data2 [10 12 6 5 [[7 6] [12 6] [7 11]]]
-        data3 [16 14 5 6 [[6 7] [18 7] [6 19]]]
-        data4 [14 12 5 6 [[6 7] [15 7] [6 16]]]]
-    ;(test-arr data2)
-    ;(test-arr data1)
-    ;(test-arr data3)
-    ;(test-arr data4)
-    ;(test-arr [10 8 0 0 [[5 2] [12 2] [5 9]]])
-    ;(test-arr [10 8 0 0 [[5 2] [15 2] [5 12]]])
-    (test-arr [8 8 2 2 [[4 3] [7 3] [4 6]]])
-    (test-arr [8 8 2 2 [[4 3] [8 3] [4 7]]])
-    (test-arr [8 8 2 2 [[4 3] [9 3] [4 8]]])
-    (test-arr [8 8 2 2 [[4 3] [10 3] [4 9]]])
-    (test-arr [8 8 2 2 [[4 3] [11 3] [4 10]]])
-    (test-arr [8 8 2 2 [[4 3] [12 3] [4 11]]])
-    (test-arr [8 8 2 2 [[4 3] [13 3] [4 12]]])
-    (test-arr [8 8 2 2 [[4 3] [14 3] [4 13]]])
-    (test-arr [8 8 2 2 [[4 3] [15 3] [4 14]]])))
-
 (defmethod compute-intersection [true true]                 ; X-greatest vertex OUTside, Y-greatest OUTside
-  [[height width center-x center-y [[x1 y1] [x2 y2] [x3 y3]]]]
+  [corner-evenness [height width center-x center-y [[x1 y1] [x2 y2] [x3 y3]] :as arr]]
   (let [inside-overhang (inc (- x2 x1))
         outside-x-overhang (- x2 (dec height))
-        outside-y-overhang (- y3 (dec width))]
+        outside-y-overhang (- y3 (dec width))
+        opposite-corner-evenness (evenness-complement corner-evenness)]
+    (print-array arr)
+    (println corner-evenness
+             inside-overhang
+             outside-x-overhang
+             outside-y-overhang)
+    #_(println (odd-and-even corner-evenness inside-overhang)
+               (odd-and-even corner-evenness outside-x-overhang)
+               (odd-and-even corner-evenness outside-y-overhang))
     (if (> (+ outside-x-overhang outside-y-overhang)
            inside-overhang)
-      (* (- height x1)
-         (- width y1))
-      (- (dotted-triangle-flat inside-overhang)
-         (dotted-triangle-flat outside-x-overhang)
-         (dotted-triangle-flat outside-y-overhang)))))
+      (let [total-half (/ (* (- height x1)
+                             (- width y1))
+                          2)]
+        {corner-evenness          (long (Math/ceil total-half))
+         opposite-corner-evenness (long (Math/floor total-half))})
+      (let [outside-x-evenness (even-or-odd-field [x1 y1] [height y1] corner-evenness)
+            outside-y-evenness (even-or-odd-field [x1 y1] [x1 width] corner-evenness)
+            minusX (odd-and-even outside-x-evenness outside-x-overhang)
+            minusY (odd-and-even outside-y-evenness outside-y-overhang)]
+        (merge-with -
+                    (odd-and-even corner-evenness inside-overhang)
+                    minusX
+                    minusY)))))
 
 (defn rhombus-quarters-and-diagonals [x y moves]
   (let [x-left (- x moves)
@@ -143,7 +159,6 @@
     (inc (- end start))))
 
 (defn solve [[height width x y moves]]
-  (println height width x y moves)
   (let [[[quad-1-triangle quad-2-triangle quad-3-triangle quad-4-triangle]
          [x-line y-line]] (rhombus-quarters-and-diagonals x y moves)
         normalized-quad-1 [height width x y quad-1-triangle]
@@ -153,9 +168,42 @@
         normalized-quadrants-triangles [normalized-quad-1 normalized-quad-2-along-x
                                         normalized-quad-3-along-x-y normalized-quad-4-along-y]
         x-diag-intersection (solve-line x-line height)
-        y-diag-intersection (solve-line y-line width)]
-    (dec (apply + x-diag-intersection y-diag-intersection (map compute-intersection normalized-quadrants-triangles)))))
+        y-diag-intersection (solve-line y-line width)
+        possible-cells (->> normalized-quadrants-triangles (map #(vector :even %))
+                            first
+                            (apply compute-intersection)
+                            ;(map #(apply compute-intersection %))
+                            ;(apply + x-diag-intersection y-diag-intersection)
+                            ;dec
+                            )]
+    possible-cells))
 
 (defn -main [& args]
   (let [processed-input (read-and-process-input)]
-    (solve (first processed-input))))
+    (->> processed-input
+         first solve
+         ; (map solve)
+         ;(str/join "\n")
+         ;(spit "output.txt")
+         )))
+
+(defn test-all []
+  (let [data1 [12 10 5 6 [[6 7] [11 7] [6 12]]]
+        data2 [10 12 6 5 [[7 6] [12 6] [7 11]]]
+        data3 [16 14 5 6 [[6 7] [18 7] [6 19]]]
+        data4 [14 12 5 6 [[6 7] [15 7] [6 16]]]]
+    ;(test-arr data2)
+    ;(test-arr data1)
+    ;(test-arr data3)
+    ;(test-arr data4)
+    ;(test-arr [10 8 0 0 [[5 2] [12 2] [5 9]]])
+    ;(test-arr [10 8 0 0 [[5 2] [15 2] [5 12]]])
+    (test-arr [8 8 2 2 [[4 3] [7 3] [4 6]]])
+    (test-arr [8 8 2 2 [[4 3] [8 3] [4 7]]])
+    (test-arr [8 8 2 2 [[4 3] [9 3] [4 8]]])
+    (test-arr [8 8 2 2 [[4 3] [10 3] [4 9]]])
+    (test-arr [8 8 2 2 [[4 3] [11 3] [4 10]]])
+    (test-arr [8 8 2 2 [[4 3] [12 3] [4 11]]])
+    (test-arr [8 8 2 2 [[4 3] [13 3] [4 12]]])
+    (test-arr [8 8 2 2 [[4 3] [14 3] [4 13]]])
+    (test-arr [8 8 2 2 [[4 3] [15 3] [4 14]]])))
